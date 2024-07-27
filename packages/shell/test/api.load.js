2fe0f6d0827f98fbbb30b2fe5273446ec6e1559c
David Worms <david@adaltas.com>
2024-07-27 13:18:56 +0200

    test(shell): api load js convertion

---
+++ packages/shell/test/api.load.js
@@ -0,0 +1,31 @@
+import fs from "node:fs/promises";
+import os from "node:os";
+import dedent from "dedent";
+import { shell } from "../lib/index.js";
+
+describe("api.load", function () {
+  it("load relative to require.main", async function () {
+    const cwd = process.cwd();
+    process.chdir(os.tmpdir());
+    await fs.writeFile(
+      `${os.tmpdir()}/relative_module.coffee`,
+      dedent`
+    export default (params) -> params
+    `
+    );
+    const mod = await shell().load(`${os.tmpdir()}/relative_module.coffee`);
+    mod("my value").should.eql("my value");
+    process.chdir(cwd);
+    await fs.unlink(`${os.tmpdir()}/relative_module.coffee`);
+  });
+
+  it("load is not a string", async function () {
+    await shell({
+      name: "start",
+    })
+      .load({ name: "something" })
+      .should.be.rejectedWith(
+        'Invalid Load Argument: load is expecting string, got {"name":"something"}'
+      );
+  });
+});
