e4565a9e270cc74b4687a42630394e2af442e9dc
David Worms <david@adaltas.com>
2024-11-24 12:46:45 +0100

    build: latest dependencies

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,37 @@
-import scopes from "@commitlint/config-lerna-scopes";
+// import scopes from "@commitlint/config-lerna-scopes";
+import fs from "node:fs/promises";
+import { glob } from "glob";
+const pkg = await fs
+  .readFile("./package.json", { encoding: "utf8" })
+  .then((data) => JSON.parse(data));
+
+const packages = await glob(
+  pkg.workspaces.map((pattern) => `${pattern}/package.json`),
+).then((files) =>
+  Promise.all(
+    files.map((file) =>
+      fs
+        .readFile(file, { encoding: "utf8" })
+        .then((data) => JSON.parse(data))
+        .then((pkg) => pkg.name.replace("@nikitajs/", "")),
+    ),
+  ),
+);
 
 export default {
-  extends: [
-    "@commitlint/config-conventional",
-    "@commitlint/config-lerna-scopes",
-  ],
+  extends: ["@commitlint/config-conventional"],
   rules: {
     "scope-enum": async (ctx) => [
       2,
       "always",
       [
-        ...(await scopes.utils.getPackages(ctx)),
+        // ...(await scopes.utils.getPackages(ctx)),
+        ...packages,
         // Custom scopes
-        'website',
-        'release',
-        'router',
-        'help'
+        "website",
+        "release",
+        "router",
+        "help",
       ],
     ],
   },
