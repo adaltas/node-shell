2f16f93380bb4c335f8c2a2a465d07a86d3e1b8a
David Worms <david@adaltas.com>
2025-05-02 11:03:26 +0200

    fix(website): commonjs imports in homepage

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,71 +1,70 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 import {
   MIN_TABLET_MEDIA_QUERY,
   TABLET_MEDIA_QUERY,
-} from 'typography-breakpoint-constants'
-import Layout from "../components/Layout"
-import Seo from "../components/Seo"
-import Banner from "../components/Banner"
-import Section from "../components/Section"
+} from "typography-breakpoint-constants";
+import Layout from "../components/Layout";
+import Seo from "../components/Seo";
+import Banner from "../components/Banner";
+import Section from "../components/Section";
 
 // Syntax
-import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
-import js from 'react-syntax-highlighter/dist/esm/languages/prism/javascript'
-import bash from 'react-syntax-highlighter/dist/esm/languages/prism/coffeescript'
-import tomorrow from 'react-syntax-highlighter/dist/esm/styles/prism/tomorrow'
-SyntaxHighlighter.registerLanguage('javascript', js);
-SyntaxHighlighter.registerLanguage('bash', bash);
-
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import js from "react-syntax-highlighter/dist/esm/languages/prism/javascript";
+import bash from "react-syntax-highlighter/dist/esm/languages/prism/coffeescript";
+import tomorrow from "react-syntax-highlighter/dist/esm/styles/prism/tomorrow";
+SyntaxHighlighter.registerLanguage("javascript", js);
+SyntaxHighlighter.registerLanguage("bash", bash);
 
 const codeString1 = `
 npm install shell
-`.trim()
+`.trim();
 
 const codeString2 = `
 git clone http://github.com/adaltas/node-shell.git
-`.trim()
+`.trim();
 
 const feature1 = `
-const shell = require("shell")
+const { shell } = require("shell");
 const app = shell({
   name: "myapp",
   description: "My CLI application",
   options: {
-    "config": {
+    config: {
       shortcut: "c",
-      description: "Some option"
-    }
+      description: "Some option",
+    },
   },
   commands: {
-    "start": {
-      description: "Start something"
-    }
-  }
-})
-`.trim()
+    start: {
+      description: "Start something",
+    },
+  },
+});
+`.trim();
 
 const feature2 = `
 /* ... */
-const args = app.parse()
-console.log(args)
+const args = app.parse();
+console.log(args);
 
 // Run \`node myapp -c value start\`
-{ command: [ 'start' ], config: 'value' }
-`.trim()
+// { command: [ 'start' ], config: 'value' }
+`.trim();
 
 const feature3 = `
-const shell = require("shell")
+const shell = require("shell");
 const app = shell({
   /* ... */
-  commands: { "start":
-    {
+  commands: {
+    start: {
       /* ... */
-      route: './routes/start.js'
-    }
-  }
-})
-app.route()
+      route: "./routes/start.js",
+    },
+  },
+});
+app.route();
 
 /* The project structure:
 |-- /node-modules
@@ -75,10 +74,10 @@ app.route()
 |-- package.json
 |-- package-lock.json
 */
-`.trim()
+`.trim();
 
 const feature4 = `
-// Run \`node myapp help\` 
+// Run \`node myapp help\`
 NAME
     myapp - My CLI application
 
@@ -96,78 +95,82 @@ COMMANDS
 EXAMPLES
     myapp --help            Show this message
     myapp help              Show this message
-`.trim()
-
+`.trim();
 
 const styles = {
   descContainer: {
-    maxWidth: '800px',
-    margin: '0 auto',
-    textAlign: 'center',
-    marginBottom: '2rem',
+    maxWidth: "800px",
+    margin: "0 auto",
+    textAlign: "center",
+    marginBottom: "2rem",
   },
   section: {
-    '& h2': {
+    "& h2": {
       textAlign: "center",
     },
-    '> div > div': {
-      display: 'flex',
-      justifyContent: 'space-between',
-      marginBottom: '2rem',
-      '& p': {
-        margin: '1.2rem 0',
-        '&:last-child': {
-          textAlign: 'center',
-        }
+    "> div > div": {
+      display: "flex",
+      justifyContent: "space-between",
+      marginBottom: "2rem",
+      "& p": {
+        margin: "1.2rem 0",
+        "&:last-child": {
+          textAlign: "center",
+        },
       },
       [TABLET_MEDIA_QUERY]: {
-        flexDirection: 'column',
-        marginBottom: '3rem',
-        '& h3': {
-          textAlign: 'center',
+        flexDirection: "column",
+        marginBottom: "3rem",
+        "& h3": {
+          textAlign: "center",
         },
       },
       [MIN_TABLET_MEDIA_QUERY]: {
-        'h3': {
-          marginTop: '.5rem',
+        h3: {
+          marginTop: ".5rem",
         },
-        '&:nth-of-type(odd)': {
-          '& > div:nth-of-type(odd)': {
+        "&:nth-of-type(odd)": {
+          "& > div:nth-of-type(odd)": {
             width: "43%",
             order: 2,
           },
-          '& > div:nth-of-type(even)': {
+          "& > div:nth-of-type(even)": {
             width: "53%",
             order: 1,
           },
         },
-      }
+      },
     },
   },
   button: {
-    border: '1px solid #fff',
-    color: '#fff',
-    padding: '10px 30px',
-    borderRadius: '3px',
-    textDecoration: 'none',
-    '&:hover': {
-      background: 'rgba(255,255,255,.05)',
-    }
-  }
-}
+    border: "1px solid #fff",
+    color: "#fff",
+    padding: "10px 30px",
+    borderRadius: "3px",
+    textDecoration: "none",
+    "&:hover": {
+      background: "rgba(255,255,255,.05)",
+    },
+  },
+};
 
 const IndexPage = () => (
   <Layout isHome={true}>
     <Seo
       title="Shell.js - argument parsing for Node.js CLI applications"
       keywords={[
-        'shell', 'cli', 'arguments', 'parameters',
-        'router', 'parse', 'stringify',
+        "shell",
+        "cli",
+        "arguments",
+        "parameters",
+        "router",
+        "parse",
+        "stringify",
       ]}
     />
     <Banner />
     <div css={styles.homePage}>
-      <Section classes={{root: styles.section}}>
+      <Section classes={{ root: styles.section }}>
         <h2>Why Shell.js?</h2>
         <div>
           <div>
@@ -180,7 +183,7 @@ const IndexPage = () => (
                 commands and to generate help screens.
               </p>
               <p>
-                <Link to='/config/' css={styles.button}>
+                <Link to="/config/" css={styles.button}>
                   Read more
                 </Link>
               </p>
@@ -196,9 +199,12 @@ const IndexPage = () => (
           <div>
             <h3>Parse arguments</h3>
             <div>
-              <p>For the handling and adding the functionality to the application operate with the `args` object returned with the method `parse`.</p>
               <p>
-                <Link to='/api/parse/' css={styles.button}>
+                For the handling and adding the functionality to the application
+                operate with the `args` object returned with the method `parse`.
+              </p>
+              <p>
+                <Link to="/api/parse/" css={styles.button}>
                   Read more
                 </Link>
               </p>
@@ -214,9 +220,11 @@ const IndexPage = () => (
           <div>
             <h3>Organize the code with routing</h3>
             <div>
-              <p>Load and configure the router in a separate top-level module.</p>
               <p>
-                <Link to='/usage/routing/' css={styles.button}>
+                Load and configure the router in a separate top-level module.
+              </p>
+              <p>
+                <Link to="/usage/routing/" css={styles.button}>
                   Read more
                 </Link>
               </p>
@@ -232,9 +240,13 @@ const IndexPage = () => (
           <div>
             <h3>Auto generate help</h3>
             <div>
-              <p>Shell.js convert the configuration object into a readable documentation string about how to use the CLI application or one of its commands.</p>
               <p>
-                <Link to='/api/help/' css={styles.button}>
+                Shell.js convert the configuration object into a readable
+                documentation string about how to use the CLI application or one
+                of its commands.
+              </p>
+              <p>
+                <Link to="/api/help/" css={styles.button}>
                   Read more
                 </Link>
               </p>
@@ -269,6 +281,6 @@ const IndexPage = () => (
       </Section>
     </div>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
